dbeb2576e21ff53c600f99c98ed4a31f029238ad
Dorian1101 <dw114222@ohio.edu>
2024-01-23 14:04:08 -0500

    finally done

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Dorian Williams>
+ *        Date: <January 23 2024>
+ * Description: git-lab-project
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
